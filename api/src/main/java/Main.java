c9887843e755521153796b0af65313363d7ad414
Mehanik <mehanik380@yahoo.com>
2024-04-26 11:11:18 +0400

    Finished version

--- api/src/main/java/Main.java
+++ api/src/main/java/Main.java
@@ -1,30 +1,113 @@
-
 import com.opencsv.CSVWriter;
-import java.io.FileNotFoundException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws ParserConfigurationException, TransformerException {
         Manipulation manipulation = new Manipulation();
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
         String fileName = "data.csv";
-        String jsonFileName= "data.json";
+        String jsonFileName = "data.json";
+        String jsonFileNameXml = "dataa.json";
+        String fileNameXml = "data.xml";
         String[] employeeInfo1 = "1,John,Smith,USA,25".split(",");
         String[] employeeInfo2 = "2,Ivan,Petrov,RU,23".split(",");
-
+        //записываем файл CSV
         try (CSVWriter writer = new CSVWriter(new FileWriter(fileName))) {
-            writer.writeNext(employeeInfo1 );
+            writer.writeNext(employeeInfo1);
             writer.writeNext(employeeInfo2);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        //создаем  пустой файл XML
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.newDocument();
+
+        //создаем теги и сохраняем в них данные
+        Element rootElement = doc.createElement("staff");
+        doc.appendChild(rootElement);
+        Element employee1 = doc.createElement("employee");
+        rootElement.appendChild(employee1);
+        Element id1 = doc.createElement("id");
+        id1.appendChild(doc.createTextNode("1"));
+        employee1.appendChild(id1);
+        Element firstName1 = doc.createElement("firstName");
+        firstName1.appendChild(doc.createTextNode("John"));
+        employee1.appendChild(firstName1);
+        Element lastName1 = doc.createElement("lastName");
+        lastName1.appendChild(doc.createTextNode("Smith"));
+        employee1.appendChild(lastName1);
+        Element country1 = doc.createElement("country");
+        country1.appendChild(doc.createTextNode("USA"));
+        employee1.appendChild(country1);
+        Element age1 = doc.createElement("age");
+        age1.appendChild(doc.createTextNode("25"));
+        employee1.appendChild(age1);
+
+        //создаем теги и сохраняем в них данные
+        Element employee2 = doc.createElement("employee");
+        rootElement.appendChild(employee2);
+        Element id2 = doc.createElement("id");
+        id2.appendChild(doc.createTextNode("2"));
+        employee2.appendChild(id2);
+        Element firstName2 = doc.createElement("firstName");
+        firstName2.appendChild(doc.createTextNode("Ivan"));
+        employee2.appendChild(firstName2);
+        Element lastName2 = doc.createElement("lastName");
+        lastName2.appendChild(doc.createTextNode("Petrov"));
+        employee2.appendChild(lastName2);
+        Element country2 = doc.createElement("country");
+        country2.appendChild(doc.createTextNode("RU"));
+        employee2.appendChild(country2);
+        Element age2 = doc.createElement("age");
+        age2.appendChild(doc.createTextNode("23"));
+        employee2.appendChild(age2);
+
+        //записываем данные в файл XML
+        DOMSource domSource = new DOMSource(doc);
+        StreamResult streamResult = new StreamResult(new File(fileNameXml));
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        transformer.setOutputProperty("indent", "yes");//включаем отступы
+        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");//добавляем пробелы для каждого уровня
+        transformer.transform(domSource, streamResult);
+
+
+        // преобразовываем файл CVS в список
         List<Employee> employees = manipulation.parseCSV(fileName, columnMapping);
+        //преобразовываем список в json
         String json = Manipulation.listToJson(employees);
+        //записываем в файл json
         Manipulation.toJson(json, jsonFileName);
 
+        // преобразовываем файл xnl в список
+        List<Employee> employeesFromXML = manipulation.parseXML(fileNameXml);
+        //преобразовываем список в json
+        String jn = Manipulation.listToJson(employeesFromXML);
+        //записываем в файл json
+        Manipulation.toJson(jn, jsonFileNameXml);
+        String s = Manipulation.readString("dataa.json");
+        List<Employee> list = Manipulation.jsonToList(s);
+       //выводим в консоль содержание списка list
+        for (Employee employee : list) {
+            System.out.println(employee.toString());
+        }
     }
+
 }
--- mp/src/main/java/Manipulation.java
+++ mp/src/main/java/Manipulation.java
@@ -1,31 +1,39 @@
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.google.gson.*;
 import com.google.gson.reflect.TypeToken;
 import com.opencsv.CSVReader;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.*;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Manipulation {
 
-
-
-
+    //конвертируем файл CVS  в json и создаем из них список employee
     public List<Employee> parseCSV(String fileName, String[] columnMapping) {
+        //создаем объект reader для чтения файла
         try (CSVReader reader = new CSVReader(new FileReader(fileName))) {
+            //создаем объект strategy для создания столбцов
             ColumnPositionMappingStrategy<Employee> strategy = new ColumnPositionMappingStrategy<>();
+            //устанавливаем тип объекта
             strategy.setType(Employee.class);
+            //установка маппинга столбцов csv
             strategy.setColumnMapping(columnMapping);
-
+            //создаем объект csv
             CsvToBean<Employee> csv = new CsvToBeanBuilder<Employee>(reader)
+                    //установка стратегии маппинга
                     .withMappingStrategy(strategy)
                     .build();
+            //парсинг cvs файла и сосдание списка employees
             List<Employee> employees = csv.parse();
             return employees;
         } catch (IOException e) {
@@ -34,19 +42,125 @@ public class Manipulation {
         return null;
     }
 
+    //преобразовываем список в файл json
     public static String listToJson(List<Employee> employees) {
+        //создаем объект gson для сериализации объектов
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        Type listType = new TypeToken<List<Employee>>() {}.getType();
+        //создаем объект  listType для предоставления типа данных Employee
+        Type listType = new TypeToken<List<Employee>>() {
+        }.getType();
+        //преобразовываем список employees в  json  строку
         String json = gson.toJson(employees, listType);
 
         return json;
     }
 
+    //записываем данные в файл
     public static void toJson(String json, String filePath) {
+        //создаем объекг fileWrriter для чтения файла
         try (FileWriter fileWriter = new FileWriter(filePath)) {
+            //читаем файл
             fileWriter.write(json);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    //получение списка из документа xml
+    public static List<Employee> parseXML(String xmlFilePath) {
+        List<Employee> employees = new ArrayList<>();
+        int id = 0;
+        String firstName = "";
+        String lastName = "";
+        String country = "";
+        int age = 0;
+        try {
+            //создаем объекты factory,builder,doc
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(new File(xmlFilePath));
+            //получаем список элементов
+            NodeList nodeList = doc.getDocumentElement().getChildNodes();
+            //перебираем список элементов
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+                if (node instanceof Element) {
+                    Element element = (Element) node;
+                    // Извлекаем значения из дочерних элементов
+                    NodeList childNodes = element.getChildNodes();
+                    for (int j = 0; j < childNodes.getLength(); j++) {
+                        Node childNode = childNodes.item(j);
+                        if (childNode instanceof Element) {
+                            Element childElement = (Element) childNode;
+                            switch (childElement.getNodeName()) {
+                                case "id":
+                                    id = Integer.parseInt(childElement.getTextContent());
+                                    break;
+                                case "firstName":
+                                    firstName = childElement.getTextContent();
+                                    break;
+                                case "lastName":
+                                    lastName = childElement.getTextContent();
+                                    break;
+                                case "country":
+                                    country = childElement.getTextContent();
+                                    break;
+                                case "age":
+                                    age = Integer.parseInt(childElement.getTextContent());
+                                    break;
+                            }
+                        }
+                    }
+                    // Создаем новый объект типа Employee
+                    Employee employee = new Employee(id, firstName, lastName, country, age);
+                    // Добавляем созданный объект в список employees
+                    employees.add(employee);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return employees;
+    }
+
+    public static String readString(String fileName) {
+        //создаем объект  sb
+        StringBuilder sb = new StringBuilder();
+        //создаем объект br для записи файла
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            //читаем файл и записываем строку
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sb.toString();
+    }
+
+    //преобразовываем json данные в java объекты
+    public static List<Employee> jsonToList(String json) {
+        List<Employee> employees = new ArrayList<>();
+        Gson gson = new Gson();// создаем объект gson
+        JsonParser parser = new JsonParser(); //создаем объект parser
+        //преобразуем json в объект jsonArray
+        JsonArray jsonArray = parser.parse(json).getAsJsonArray();
+
+        for (JsonElement element : jsonArray) { //проходим по всем элементам jsonArray
+            JsonObject jsonObject = element.getAsJsonObject();
+            //извлекаем данные и преобразуем в String
+            long id = jsonObject.get("id").getAsLong();
+            String firstName = jsonObject.get("firstName").getAsString();
+            String lastName = jsonObject.get("lastName").getAsString();
+            String country = jsonObject.get("country").getAsString();
+            int age = jsonObject.get("age").getAsInt();
+            //создаем новый объект с полученными значениями
+            Employee employee = new Employee(id, firstName, lastName, country, age);
+            //добавляем созданный объект в список employees
+            employees.add(employee);
+        }
+        return employees;
     }
 }
